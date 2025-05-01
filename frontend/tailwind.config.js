0d490d432464aed53fecb76ac172a11f8e916141
asafeeson <g.kochergin@hotmail.com>
2025-05-01 20:24:07 +0200

    - postcss setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,20 @@
 // tailwind.config.js
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          primary: '#01DA01',
-          secondary: '#01BC01',
-          'style-title': '#001C32',
-          'style-subtitle': '#A3A3A3',
-          'accent-lightblue': '#E2EDFF',
-          'accent-blue': '#0F61EF',
-        },
-        fontFamily: {
-          sans: ['Manrope', 'sans-serif'],
-          heading: ['Oswald', 'sans-serif']
-        }
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#01DA01',
+        secondary: '#01BC01',
+        'style-title': '#001C32',
+        'style-subtitle': '#A3A3A3',
+        'accent-lightblue': '#E2EDFF',
+        'accent-blue': '#0F61EF',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+        heading: ['Oswald', 'sans-serif']
       }
     }
   }
+}
