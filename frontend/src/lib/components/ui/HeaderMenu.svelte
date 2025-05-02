7fe9075430db5818349d98f3c2bac123c1260496
asafeeson <g.kochergin@hotmail.com>
2025-05-02 12:23:34 +0200

    - refactor

--- frontend/src/lib/components/ui/HeaderMenu.svelte
+++ frontend/src/lib/components/ui/HeaderMenu.svelte
@@ -5,7 +5,6 @@
 	let { links }: { links: IMenuItem[] } = $props();
 	console.log(links);
 	function comparePathname(url: string): boolean {
-		console.log(page.url.pathname);
 		return page.url.pathname == url;
 	}
 </script>
@@ -17,14 +16,10 @@
 				<a
 					href={link.slug}
 					class="hover:text-primary text-15px text-black"
-					class:active-link={() => comparePathname(link.slug)}
 					title={link.name}
 					aria-label={link.name}
 				>
 					{link.name}
-					{link.id}
-					{link.slug}
-					{page.url.pathname}
 				</a>
 			</li>
 		{/each}
