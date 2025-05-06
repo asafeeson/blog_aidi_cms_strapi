33d9b24a3ff06969fef5c82aeaf1fc2a4a564966
asafeeson <g.kochergin@hotmail.com>
2025-05-06 10:51:40 +0200

    refactor

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,27 +15,3 @@ export default {
 	plugins: []
 } satisfies Config;
 
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     './src/**/*.{html,js,svelte,ts}', // всё, что нужно Tailwind для поиска классов
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: '#01DA01',
-//         secondary: '#01BC01',
-//         'style-title': '#001C32',
-//         'style-subtitle': '#A3A3A3',
-//         'accent-blue': '#0F61EF',
-//         lightblue: '#E2EDFF',
-//         page: '#F7F7F7' // 👈 вот это добавь
-//       },
-//       fontFamily: {
-//         sans: ['Manrope', 'sans-serif'],
-//         heading: ['Oswald', 'sans-serif'],
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
